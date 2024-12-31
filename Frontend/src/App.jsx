6c077389ac7a0d60604056e1870ee36f51ce5944
Chamod Perera <chamod914@gmail.com>
2024-12-31 15:43:13 +0530

    Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,15 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='font-poppins'>
+        <div className=' bg-[#1E88E5] h-96 rounded-b-3xl text-center flex flex-col justify-center items-center'>
+          <h1 className=' text-8xl font-MeowScript text-[#FFC107] font-bold tracking-wider mb-2 [-webkit-text-stroke:1p_black]'>Book Sphere</h1>
+          <p className='text-white text-xl [-webkit-text-stroke:0.1px_black]'>A Universe of Books in Your Hands.</p>
+        </div>
+
+        <div>
+
+        </div>
 
       </div>
         
